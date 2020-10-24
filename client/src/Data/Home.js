41e755771f45353dad99171d6ae0e1bc9c56593f
Gaius Julius Caesar <hollismaximilian@gmail.com>
2020-10-24 01:02:34 -0400

    added input

---
+++ client/src/Data/Home.js
@@ -0,0 +1,89 @@
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Maskify',
+  headline: 'Live Compliance Information',
+  description:
+    'We provide small businesses with all the data they need to ensure mask usage compliance.',
+  buttonLabel: 'Get Started',
+  imgStart: '',
+  img: require('../images/maskify-landing-2.svg'),
+  alt: 'Credit Card',
+  start: '',
+  backgroundImg: require('../images/mntn.svg'),
+};
+
+export const homeObjTwo = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Instant Setup',
+  headline: 'Extremely quick onboarding process',
+  description:
+    "Once you've joined, we'll walk you through all our services and get you set up in minutes.",
+  buttonLabel: 'Learn More',
+  imgStart: '',
+  img: require('../images/svg-2.svg'),
+  alt: 'Vault',
+  start: '',
+  backgroundImg: require('../images/pricing-landing.svg'),
+};
+
+export const homeObjThree = {
+  primary: false,
+  lightBg: true,
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Maskify Dashboard',
+  headline:
+    'Dashboard provides for all your data needs!',
+  description:
+    "Our Neural Engine processes your camera feeds live. See what it can do for you.",
+  buttonLabel: 'View Case Study',
+  imgStart: 'start',
+  img: require('../images/mashup.png'),
+  alt: 'Vault',
+  start: 'true',
+  backgroundImg: require('../images/onboarding.svg'),
+};
+
+export const homeObjFour = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Secure Database',
+  headline: 'All your data is stored on our secure server',
+  description:
+    'All our data is processed and stored on a reliable ubuntu server, secured by ssh authentication. Your records and data our safe with us. All live feeds processed locally on your own hardware.',
+  buttonLabel: 'Sign Up Now',
+  imgStart: 'start',
+  img: require('../images/svg-3.svg'),
+  alt: 'Vault',
+  start: 'true',
+  backgroundImg: require('../images/data.svg'),
+};
+
+export const subscriptionObj = {
+  primary: true,
+  lightBg: false,
+  //lightTopLine: true,
+  //lightText: true,
+  //lightTextDesc: true,
+  //topLine: 'Secure Database',
+  //headline: 'All your data is stored on our secure server',
+  //description:
+  //  'You will never have to worry about your information getting leaked. Our team of security experts will ensure your records are kept safe.',
+  //buttonLabel: 'Sign Up Now',
+  //imgStart: 'start',
+  img: require('../images/svg-card.svg'),
+  //alt: 'Vault',
+  //start: 'true'
+}
